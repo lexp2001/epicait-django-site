1f7f6cf3c20c5ffa67033761956e0770e7c212f3
Leonardo Barreto <leonardojbarreto@gmail.com>
2020-01-09 12:16:01 +0100

    Security Key

--- djangoexample/settings.py
+++ djangoexample/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&)pb0!-w=-6w@006=ed5f5v@s5gijmw_10n9y_ut*v$n+*=8r*'
+SECRET_KEY = 'XXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
